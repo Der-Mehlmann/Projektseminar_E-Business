f433c6e9eae3d257fe8ac98747fadc492545ac61
Richard12434 <uni@oppenhorst.com>
2025-10-23 16:57:24 +0200

    Create index.js
    Java Script erste Klassen gestaltung

---
+++ index.js
@@ -0,0 +1,26 @@
+class Player {
+    level=0
+    xKoord=0
+    yKoord=0
+    constructor(hp, baseDamage, hitbox, firstWeapon,speed,range){
+        this.hp=hp
+        this.baseDamage=baseDamage
+        this.hitbox=hitbox
+        this.firstWeapon=firstWeapon
+        this.speed=speed
+        this.range=range
+    }
+
+    move(){
+
+    }
+    takeDamage(){
+
+    }
+    levelUp(){
+
+    }
+    draw(){
+        
+    }
+}
